1c8e22fa918281ba84aebc9d9ca8dfffdd6cab32
hannehaugland <hsofiehaugland@gmail.com>
2023-10-27 11:28:15 +0200

    Resolve suggestions: error handling

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -5,22 +5,34 @@ import { apiUrl } from "../constants";
 
 export default function Generator() {
   // fetching random movie
-  const { data: movie, refetch: refetchMovie } = useQuery(
-    ["Movie"],
-    async () => {
-      const res = await fetch(`${apiUrl}/api/movies/random`);
-      return await res.json();
-    }
-  );
+  const {
+    data: movie,
+    refetch: refetchMovie,
+    isError: isErrorMovie,
+    isLoading: isLoadingMovie,
+  } = useQuery(["Movie"], async () => {
+    const res = await fetch(`${apiUrl}/api/movies/random`);
+    return await res.json();
+  });
 
   // fetching random recipie
-  const { data: recipie, refetch: refetchRecipie } = useQuery(
-    ["Recipie"],
-    async () => {
-      const res = await fetch(`${apiUrl}/api/recipies/random`);
-      return await res.json();
-    }
-  );
+  const {
+    data: recipie,
+    refetch: refetchRecipie,
+    isError: isErrorRecipie,
+    isLoading: isLoadingRecipie,
+  } = useQuery(["Recipie"], async () => {
+    const res = await fetch(`${apiUrl}/api/recipies/random`);
+    return await res.json();
+  });
+
+  if (isLoadingMovie || isLoadingRecipie) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (isErrorMovie || isErrorRecipie) {
+    return <Text>Woops</Text>;
+  }
 
   return (
     <VStack gap={2}>
@@ -35,10 +47,10 @@ export default function Generator() {
       </Button>
       <br />
       <Text as="b">Middag: </Text>
-      <Text as="i">{recipie?.name ?? "Det skjedde en feil"}</Text>
+      <Text as="i">{recipie.name}</Text>
       <br />
       <Text as="b">Film: </Text>
-      <Text as="i">{movie?.name ?? "Det skjedde en feil"}</Text>
+      <Text as="i">{movie.name}</Text>
     </VStack>
   );
 }
